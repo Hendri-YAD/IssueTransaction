7b07de67466ef2307bddaad03d68c4f8ca4ee0c1
Hendri <hendrisetiawardana@gmail.com>
2018-03-21 20:08:03 +0800

    2

---
+++ Return.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace IssueTran
+{
+    partial class Return
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb_transactionID = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.DTP_DateReturn = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(132, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(168, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Transaction ID";
+            // 
+            // tb_transactionID
+            // 
+            this.tb_transactionID.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_transactionID.Location = new System.Drawing.Point(322, 44);
+            this.tb_transactionID.Name = "tb_transactionID";
+            this.tb_transactionID.Size = new System.Drawing.Size(73, 34);
+            this.tb_transactionID.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(160, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(140, 29);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Date Return";
+            // 
+            // DTP_DateReturn
+            // 
+            this.DTP_DateReturn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DTP_DateReturn.Location = new System.Drawing.Point(322, 106);
+            this.DTP_DateReturn.Name = "DTP_DateReturn";
+            this.DTP_DateReturn.Size = new System.Drawing.Size(390, 34);
+            this.DTP_DateReturn.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(272, 189);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 47);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Return
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(824, 296);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.DTP_DateReturn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tb_transactionID);
+            this.Controls.Add(this.label1);
+            this.Name = "Return";
+            this.Text = "Return";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb_transactionID;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker DTP_DateReturn;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ Return.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IssueTran
+{
+    public partial class Return : Form
+    {
+        public Return()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DafestyEntities context = new DafestyEntities();
+            IssueTran i = context.IssueTrans.
+                Where(x => x.TransactionID.ToString() == tb_transactionID.Text).First();
+            i.RentalStatus = "in";
+            i.DateActualReturn = DTP_DateReturn.Value;
+
+            Movie m = context.Movies.
+                Where(x => x.VideoCode == i.VideoCode).First();
+            m.NumberRented--;
+
+            context.SaveChanges();
+        }
+    }
+}
